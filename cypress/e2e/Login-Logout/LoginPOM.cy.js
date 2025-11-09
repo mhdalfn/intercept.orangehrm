3a2f7a805ea198c5be851e91771bfb4e28072a38
Muhammad Alfian <95150867+mhdalfn@users.noreply.github.com>
2025-11-09 07:03:05 +0700

    update loginpom

--- cypress/e2e/Login-Logout/LoginPOM.cy.js
+++ cypress/e2e/Login-Logout/LoginPOM.cy.js
@@ -1,61 +1,54 @@
 import LoginPage from '../../pages/LoginPage'
 import { LoginData } from '../../data/LoginData'
 
-describe('Login Test Suite with POM + Intercept', () => {
+describe('Login Test Suite', () => {
 
     it('Success Login', () => {
         LoginPage.interceptDashboard()
         LoginPage.login(LoginData.validUser.username, LoginData.validUser.password)
         cy.wait('@getLocations')
-        cy.url().should('contain', '/dashboard')
-        cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').should('be.visible')
+        LoginPage.verifyDashboardPage()
     })
 
     it('Failed Login with Invalid Username', () => {
         LoginPage.interceptLoginPage()
         LoginPage.login(LoginData.invalidUser.username, LoginData.invalidUser.password)
         cy.wait('@getLoginPage')
-        cy.url().should('include', '/login')
-        cy.get('.oxd-button').should('be.visible')
+        LoginPage.verifyLoginPage()
     })
 
     it('Failed Login with Invalid Password', () => {
         LoginPage.interceptMessages('invalidPassMsg')
         LoginPage.login(LoginData.invalidPassword.username, LoginData.invalidPassword.password)
         cy.wait('@invalidPassMsg')
-        cy.url().should('include', '/login')
-        cy.get('.oxd-button').should('be.visible')
+        LoginPage.verifyLoginPage()
     })
 
     it('Failed Login with Empty Username', () => {
         LoginPage.interceptMessages('emptyUserMsg')
         LoginPage.login(LoginData.emptyUser.username, LoginData.emptyUser.password)
         cy.wait('@emptyUserMsg')
-        cy.url().should('include', '/login')
-        cy.get('.oxd-button').should('be.visible')
+        LoginPage.verifyLoginPage()
     })
 
     it('Failed Login with Empty Password', () => {
         LoginPage.interceptMessages('emptyPassMsg')
         LoginPage.login(LoginData.emptyPassword.username, LoginData.emptyPassword.password)
         cy.wait('@emptyPassMsg')
-        cy.url().should('include', '/login')
-        cy.get('.oxd-button').should('be.visible')
+        LoginPage.verifyLoginPage()
     })
 
     it('Failed Login with Empty Username & Password', () => {
         LoginPage.interceptMessages('emptyBothMsg')
         LoginPage.login(LoginData.emptyBoth.username, LoginData.emptyBoth.password)
         cy.wait('@emptyBothMsg')
-        cy.url().should('include', '/login')
-        cy.get('.oxd-button').should('be.visible')
+        LoginPage.verifyLoginPage()
     })
 
     it('Failed Login with Uppercase Username & Password', () => {
         LoginPage.interceptMessages('upperMsg')
         LoginPage.login(LoginData.uppercaseUserPass.username, LoginData.uppercaseUserPass.password)
         cy.wait('@upperMsg')
-        cy.url().should('include', '/login')
-        cy.get('.oxd-button').should('be.visible')
+        LoginPage.verifyLoginPage()
     })
 })
